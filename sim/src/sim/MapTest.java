9ddcef6435373e6fc9901e663e44c2404f6a9704
Jewoo Lee <shema7k@gmail.com>
2019-11-24 19:01:35 +0900

    add more tests for sim/Map

--- sim/src/sim/MapTest.java
+++ sim/src/sim/MapTest.java
@@ -1,5 +1,7 @@
 package sim;
 
+import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.RepeatedTest;
@@ -7,10 +9,10 @@ import org.junit.jupiter.api.RepetitionInfo;
 import org.junit.jupiter.api.Test;
 
 class MapTest {
-	
+
 	// Used for single-map tests. Arbitrarily chosen.
 	final int TEST_MAP_WIDTH = 6;
-	final int TEST_MAP_HEIGHT = 7;	
+	final int TEST_MAP_HEIGHT = 7;
 
 	@RepeatedTest(100)
 	void mapConstructionTest(RepetitionInfo repetitionInfo) {
@@ -37,7 +39,7 @@ class MapTest {
 		assertEquals(w, map.width());
 		assertEquals(h, map.height());
 	}
-	
+
 	@Test
 	void mapGetThrowTest() {
 		Map map = new Map(TEST_MAP_WIDTH, TEST_MAP_HEIGHT);
@@ -57,13 +59,62 @@ class MapTest {
 			}
 		}
 	}
-	
+
 	@Test
 	void initialMapContentsTest() {
 		Map map = new Map(TEST_MAP_WIDTH, TEST_MAP_HEIGHT);
 		for (int x = 0; x < TEST_MAP_WIDTH; x++) {
 			for (int y = 0; y < TEST_MAP_HEIGHT; y++) {
 				assertEquals(map.get(x, y), Cell.EMPTY);
+			}
+		}
+	}
+	
+	@Test
+	void mapSetThrowTest() {
+		Map map = new Map(TEST_MAP_WIDTH, TEST_MAP_HEIGHT);
+		for (int _x = -3; _x < TEST_MAP_WIDTH + 3; _x++) {
+			for (int _y = -3; _y < TEST_MAP_HEIGHT + 3; _y++) {
+				final int x = _x;
+				final int y = _y;
+				if (x < 0 || x >= TEST_MAP_WIDTH || y < 0 || y >= TEST_MAP_HEIGHT) {
+					for (Cell _c: Cell.values()) {
+						final Cell c = _c;
+						assertThrows(IndexOutOfBoundsException.class, () -> {
+							map.set(x, y, c);
+						});
+					}
+				} else {
+					for (Cell _c: Cell.values()) {
+						final Cell c = _c;
+						assertDoesNotThrow(() -> {
+							map.set(x, y, c);
+						});
+					}
+				}
+			}
+		}
+	}
+	
+	@RepeatedTest(100)
+	void mapGetSetTest(RepetitionInfo repetitionInfo) {
+		Map map = new Map(TEST_MAP_WIDTH, TEST_MAP_HEIGHT);
+		Cell[] cells = Cell.values();
+		final long SEED = (long)repetitionInfo.getCurrentRepetition();
+		
+		Random rng = new Random(SEED);
+		for (int x = 0; x < TEST_MAP_WIDTH; x++) {
+			for (int y = 0; y < TEST_MAP_HEIGHT; y++) {
+				int i = rng.nextInt(cells.length);
+				map.set(x, y, cells[i]);
+			}
+		}
+		
+		rng.setSeed(SEED);
+		for (int x = 0; x < TEST_MAP_WIDTH; x++) {
+			for (int y = 0; y < TEST_MAP_HEIGHT; y++) {
+				int i = rng.nextInt(cells.length);
+				assertEquals(map.get(x, y), cells[i]);
 			}
 		}
 	}
