63391a9beaa6f0da6982cc861adf101ee9f3b108
Jewoo Lee <shema7k@gmail.com>
2019-11-28 10:30:46 +0900

    implement Map

--- addon/src/seproj/shrimpsnack/addon/map/Map.java
+++ addon/src/seproj/shrimpsnack/addon/map/Map.java
@@ -7,13 +7,31 @@ public class Map {
 	private Pair size;
 
 	public Map(Pair size) {
+
+		if (size.x <= 0) {
+			throw new IllegalArgumentException(String.format("Illegal Width: %d", size.x));
+		}
+
+		if (size.y <= 0) {
+			throw new IllegalArgumentException(String.format("Illegal Height: %d", size.y));
+		}
+
 		this.size = size;
+		this.cells = new Cell[size.x * size.y];
 	}
 
 	public Cell get(Pair pos) {
+		if (pos.x < 0 || this.size.x <= pos.x || pos.y < 0 || this.size.y <= pos.y) {
+			throw new IndexOutOfBoundsException(
+					String.format("Coordinates (%d, %d) out of bounds for map size (%d, %d)", pos.x, pos.y, this.size.x,
+							this.size.y));
+		}
+
+		return cells[pos.y * this.size.x + pos.x];
 	}
 
 	public Pair getSize() {
+		return this.size;
 	}
 
 }
