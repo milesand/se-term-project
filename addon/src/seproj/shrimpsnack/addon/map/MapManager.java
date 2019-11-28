2f79e1b78f1dbb74fab627c2c038e4b124dd72f0
Jewoo Lee <shema7k@gmail.com>
2019-11-28 11:22:35 +0900

    implement markBlobs

--- addon/src/seproj/shrimpsnack/addon/map/MapManager.java
+++ addon/src/seproj/shrimpsnack/addon/map/MapManager.java
@@ -2,6 +2,7 @@ package seproj.shrimpsnack.addon.map;
 
 import seproj.shrimpsnack.addon.sim.SIMConnection;
 import seproj.shrimpsnack.addon.utility.Direction;
+import seproj.shrimpsnack.addon.utility.OptionalBool;
 import seproj.shrimpsnack.addon.utility.Pair;
 
 public class MapManager {
@@ -10,7 +11,39 @@ public class MapManager {
 	private Pair pos;
 	private Direction dir;
 
-	public void mark_blobs() {
+	public void mark_blobs() throws Exception {
+		Pair pos = this.position();
+
+		Cell[] unknown_adj_cells = new Cell[4];
+		boolean found_unknown = false;
+
+		Direction dir = Direction.N;
+		for (int i = 0; i < 4; i++) {
+			try {
+				Pair adj_pos = new Pair(pos.x + dir.x(), pos.y + dir.y());
+				Cell adj_cell = this.map.get(adj_pos);
+				if (adj_cell.isBlob().equals(OptionalBool.Unknown)) {
+					unknown_adj_cells[i] = adj_cell;
+					found_unknown = true;
+				}
+			} catch (IndexOutOfBoundsException e) {
+				continue;
+			} finally {
+				dir = dir.nextClockwise();
+			}
+		}
+
+		if (!found_unknown) {
+			return;
+		}
+
+		boolean[] blobs = this.sim.detectBlob();
+		for (int i = 0; i < 4; i++) {
+			if (unknown_adj_cells[i] == null) {
+				continue;
+			}
+			unknown_adj_cells[i].setBlob(blobs[i]);
+		}
 	}
 
 	public boolean checkHazard() {
@@ -26,9 +59,13 @@ public class MapManager {
 	}
 
 	public void invalidatePosition() {
+		this.pos = null;
 	}
 
-	public Pair position() {
+	public Pair position() throws Exception {
+		if (this.pos == null) {
+			this.pos = this.sim.getPosition();
+		}
 		return this.pos;
 	}
 
--- addon/src/seproj/shrimpsnack/addon/utility/Direction.java
+++ addon/src/seproj/shrimpsnack/addon/utility/Direction.java
@@ -1,5 +1,66 @@
 package seproj.shrimpsnack.addon.utility;
 
 public enum Direction {
-	N, E, S, W
+	N {
+		public int x() {
+			return 0;
+		};
+
+		public int y() {
+			return 1;
+		}
+
+		@Override
+		public Direction nextClockwise() {
+			return E;
+		}
+	},
+	E {
+		public int x() {
+			return 1;
+		};
+
+		public int y() {
+			return 0;
+		}
+
+		@Override
+		public Direction nextClockwise() {
+			return S;
+		}
+	},
+	S {
+		public int x() {
+			return 0;
+		};
+
+		public int y() {
+			return -1;
+		}
+
+		@Override
+		public Direction nextClockwise() {
+			return W;
+		}
+	},
+	W {
+		public int x() {
+			return -1;
+		};
+
+		public int y() {
+			return 0;
+		}
+
+		@Override
+		public Direction nextClockwise() {
+			return N;
+		}
+	};
+
+	public abstract int x();
+
+	public abstract int y();
+
+	public abstract Direction nextClockwise();
 }
