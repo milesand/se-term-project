cfa3aa0710e1407b855a33567068e1cc11dbd15f
Jewoo Lee <shema7k@gmail.com>
2019-12-04 14:32:20 +0900

    refactor SimProcess
    This commit changes three major things:
    1. Addition of CLOSE command, to allow clean exit.
    2. .run() is now separate from main(), to allow easier unit testing without networking.
    3. main() now spawns a thread for each incoming connection, instead of just accepting one
       and stopping listening.

--- sim/src/seproj/shrimpsnack/sim/SimProcess.java
+++ sim/src/seproj/shrimpsnack/sim/SimProcess.java
@@ -4,7 +4,10 @@ import java.io.BufferedInputStream;
 import java.io.DataInputStream;
 import java.io.BufferedOutputStream;
 import java.io.DataOutputStream;
+import java.io.EOFException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.HashSet;
@@ -12,7 +15,7 @@ import java.util.Random;
 
 public class SimProcess {
 
-	enum Command {
+	static enum Command {
 		// 0
 		SET_MAP_SIZE, SET_BOT_POS, SET_BOT_DIR, ADD_HAZARD, REMOVE_HAZARD,
 		// 5
@@ -22,7 +25,9 @@ public class SimProcess {
 
 		GET_BOT_DIR, GET_MAP_SIZE, GET_BOT_POS, DETECT_HAZARD,
 		// 15
-		DETECT_BLOBS, MOVE_FORWARD, TURN_CW;
+		DETECT_BLOBS, MOVE_FORWARD, TURN_CW,
+
+		CLOSE;
 	}
 
 	final static Command[] COMMANDS = Command.values();
@@ -31,7 +36,23 @@ public class SimProcess {
 	final static byte OK = 0;
 	final static byte ERR = 1;
 
-	public static void main(String[] args) {
+	final DataInputStream in;
+	final DataOutputStream out;
+
+	SimProcess(InputStream in, OutputStream out) {
+		this.in = new DataInputStream(new BufferedInputStream(in));
+		this.out = new DataOutputStream(new BufferedOutputStream(out));
+	}
+
+	private void run() throws IOException {
+		SimImpl sim = this.handle_builder_request();
+		if (sim == null) {
+			return;
+		}
+		this.handle_sim_request(sim);
+	}
+
+	public static void main(String[] args) throws IOException {
 		if (args.length == 0) {
 			System.err.println("Error: Missing port argument");
 			return;
@@ -45,22 +66,28 @@ public class SimProcess {
 			return;
 		}
 
-		// We'll deal with just one connection, for simplicity.
-		try (ServerSocket listener = new ServerSocket(port); Socket conn = listener.accept();) {
-
-			DataInputStream in = new DataInputStream(new BufferedInputStream(conn.getInputStream()));
-			DataOutputStream out = new DataOutputStream(new BufferedOutputStream(conn.getOutputStream()));
-
-			SimImpl sim = handle_builder_request(in, out);
-			handle_sim_request(in, out, sim);
-
-		} catch (IOException e1) {
-			e1.printStackTrace();
-			return;
+		try (ServerSocket listener = new ServerSocket(port)) {
+			while (true) {
+				Socket conn = listener.accept();
+				Thread t = new Thread(() -> {
+					try {
+						new SimProcess(conn.getInputStream(), conn.getOutputStream()).run();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+					try {
+						conn.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+					return;
+				});
+				t.run();
+			}
 		}
 	}
 
-	static SimImpl handle_builder_request(DataInputStream in, DataOutputStream out) throws IOException {
+	private SimImpl handle_builder_request() throws IOException {
 		// Create a builder, and two sets for storing coordinates of places-of-interest,
 		// and RNG seed.
 		SimImpl.Builder builder = new SimImpl.Builder();
@@ -68,71 +95,84 @@ public class SimProcess {
 		HashSet<SimImpl.Coordinates> blobs = new HashSet<SimImpl.Coordinates>();
 		Long seed = null;
 
+		byte command;
 		while (true) {
-			for (byte command = in.readByte(); COMMANDS[command] != Command.BUILD; command = in.readByte()) {
+			while (true) {
+				try {
+					command = this.in.readByte();
+				} catch (EOFException e) {
+					return null;
+				}
 				try {
 					if (command >= COMMANDS.length) {
 						throw new IllegalArgumentException();
 					}
+					if (COMMANDS[command].equals(Command.BUILD)) {
+						break;
+					}
 					switch (COMMANDS[command]) {
 					case SET_MAP_SIZE:
-						int w = in.readInt();
-						int h = in.readInt();
+						int w = this.in.readInt();
+						int h = this.in.readInt();
 						builder.mapSize(w, h);
 						break;
 					case SET_BOT_POS:
-						int x = in.readInt();
-						int y = in.readInt();
+						int x = this.in.readInt();
+						int y = this.in.readInt();
 						builder.robotPosition(x, y);
 						break;
 					case SET_BOT_DIR:
-						byte dir_idx = in.readByte();
+						byte dir_idx = this.in.readByte();
 						if (dir_idx >= DIRECTIONS.length) {
 							throw new IllegalArgumentException();
 						}
 						builder.robotDirection(DIRECTIONS[dir_idx]);
 						break;
 					case ADD_HAZARD:
-						x = in.readInt();
-						y = in.readInt();
+						x = this.in.readInt();
+						y = this.in.readInt();
 						hazards.add(new SimImpl.Coordinates(x, y));
 						break;
 					case REMOVE_HAZARD:
-						x = in.readInt();
-						y = in.readInt();
+						x = this.in.readInt();
+						y = this.in.readInt();
 						hazards.remove(new SimImpl.Coordinates(x, y));
 						break;
 					case ADD_BLOB:
-						x = in.readInt();
-						y = in.readInt();
+						x = this.in.readInt();
+						y = this.in.readInt();
 						blobs.add(new SimImpl.Coordinates(x, y));
 						break;
 					case REMOVE_BLOB:
-						x = in.readInt();
-						y = in.readInt();
+						x = this.in.readInt();
+						y = this.in.readInt();
 						blobs.remove(new SimImpl.Coordinates(x, y));
 						break;
 					case SET_SEED:
-						seed = in.readLong();
+						seed = this.in.readLong();
 						break;
 					case SET_PROB_0:
-						float prob = in.readFloat();
+						float prob = this.in.readFloat();
 						builder.setNoMovementProbability(prob);
 						break;
 					case SET_PROB_2:
-						prob = in.readFloat();
+						prob = this.in.readFloat();
 						builder.setDoubleForwardProbability(prob);
 						break;
+					case CLOSE:
+						this.out.writeByte(OK);
+						this.out.flush();
+						return null;
 					default:
 						throw new IllegalArgumentException();
 					}
 				} catch (IllegalArgumentException e) {
-					out.writeByte(ERR);
-					out.flush();
+					this.out.writeByte(ERR);
+					this.out.flush();
 					continue;
 				}
-				out.writeByte(OK);
-				out.flush();
+				this.out.writeByte(OK);
+				this.out.flush();
 			}
 
 			// If we reached here, command equaled Command.BUILD and thus we should try
@@ -146,88 +186,100 @@ public class SimProcess {
 				}
 				SimImpl sim = builder.hazards(hazards.stream()).blobs(blobs.stream()).rng(rng).build();
 				// Success: tell the user we've built it and return to caller.
-				out.writeByte(OK);
-				out.flush();
+				this.out.writeByte(OK);
+				this.out.flush();
 				return sim;
 
 			} catch (IllegalStateException | IndexOutOfBoundsException | IllegalArgumentException e) {
 				// We just signal error to user and keep looping.
-				out.writeByte(ERR);
-				out.flush();
+				this.out.writeByte(ERR);
+				this.out.flush();
 			}
 		}
 	}
 
-	static void handle_sim_request(DataInputStream in, DataOutputStream out, SimImpl sim) throws IOException {
+	private void handle_sim_request(SimImpl sim) throws IOException {
+		byte command;
 		while (true) {
-			byte command = in.readByte();
+			try {
+				command = this.in.readByte();
+			} catch (EOFException e) {
+				return;
+			}
+
 			if (command >= COMMANDS.length) {
-				out.writeByte(ERR);
-				out.flush();
+				this.out.writeByte(ERR);
+				this.out.flush();
 				continue;
 			}
+
 			switch (COMMANDS[command]) {
 
 			case GET_BOT_DIR:
 				Direction dir = sim.direction();
 				byte dir_encoded = (byte) dir.ordinal();
-				out.writeByte(OK);
-				out.writeByte(dir_encoded);
-				out.flush();
+				this.out.writeByte(OK);
+				this.out.writeByte(dir_encoded);
+				this.out.flush();
 				break;
 
 			case GET_MAP_SIZE:
 				int w = sim.map_width();
 				int h = sim.map_height();
-				out.writeByte(OK);
-				out.writeInt(w);
-				out.writeInt(h);
-				out.flush();
+				this.out.writeByte(OK);
+				this.out.writeInt(w);
+				this.out.writeInt(h);
+				this.out.flush();
 				break;
 
 			case GET_BOT_POS:
 				int x = sim.x();
 				int y = sim.y();
-				out.writeByte(OK);
-				out.writeInt(x);
-				out.writeInt(y);
-				out.flush();
+				this.out.writeByte(OK);
+				this.out.writeInt(x);
+				this.out.writeInt(y);
+				this.out.flush();
 				break;
 
 			case DETECT_HAZARD:
 				boolean hazard = sim.detectHazard();
-				out.writeByte(OK);
-				out.writeBoolean(hazard);
-				out.flush();
+				this.out.writeByte(OK);
+				this.out.writeBoolean(hazard);
+				this.out.flush();
 				break;
 
 			case DETECT_BLOBS:
 				boolean[] blobs = sim.detectBlobs();
-				out.writeByte(OK);
+				this.out.writeByte(OK);
 				for (boolean blob : blobs) {
-					out.writeBoolean(blob);
+					this.out.writeBoolean(blob);
 				}
-				out.flush();
+				this.out.flush();
 				break;
 
 			case MOVE_FORWARD:
 				if (sim.moveForward()) {
-					out.writeByte(OK);
+					this.out.writeByte(OK);
 				} else {
-					out.writeByte(ERR);
+					this.out.writeByte(ERR);
 				}
-				out.flush();
+				this.out.flush();
 				break;
 
 			case TURN_CW:
 				sim.turnClockwise();
-				out.writeByte(OK);
-				out.flush();
+				this.out.writeByte(OK);
+				this.out.flush();
 				break;
+			
+			case CLOSE:
+				this.out.writeByte(OK);
+				this.out.flush();
+				return;
 
 			default:
-				out.writeByte(ERR);
-				out.flush();
+				this.out.writeByte(ERR);
+				this.out.flush();
 				break;
 			}
 		}
